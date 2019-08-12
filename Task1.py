4c4d78dce1d97358a792fb00d40d6735731620d5
Aleksandr <Aleksandrsanya@gmail.com>
2019-08-12 23:42:03 +0300

    Lessons2

---
+++ Task1.py
@@ -0,0 +1,9 @@
+a = float(input("Enter a :"))
+b = float(input("Enter b :"))
+c = float(input("Enter c :"))
+if a >= b and a >= c :
+   print ("c",c)
+if b >= a and b >= c :
+   print ("b",b)
+if c >= a and c >= b :
+   print ("c",c)
---
+++ Task9.py
@@ -0,0 +1,6 @@
+n = int(input("Введіть число n:"))
+i = 2
+while i <= n//2:
+    if n % i ==0:
+        print(i)
+    i = i+1
